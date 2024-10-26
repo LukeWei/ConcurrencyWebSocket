6c8c800007175d794ac7913691441e634ef57b87
Luke Lin <lukeyw.lin@gmail.com>
2024-10-26 20:19:33 +0800

    Implement AsyncThrowingStream and Unit Test

--- Sources/ConcurrencyWebSocket/ConcurrencyWebSocket.swift
+++ Sources/ConcurrencyWebSocket/ConcurrencyWebSocket.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+typealias WebSocketStream = AsyncThrowingStream<URLSessionWebSocketTask.Message, Error>
+
+extension URLSessionWebSocketTask {
+    func stream() -> WebSocketStream {
+        AsyncThrowingStream { continuation in
+            Task {
+                while self.state == .running || self.state == .suspended {
+                    do {
+                        let message = try await self.receive()
+                        continuation.yield(message)
+                    } catch {
+                        continuation.finish(throwing: error)
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
--- Tests/ConcurrencyWebSocketTests/ConcurrencyWebSocketTests.swift
+++ Tests/ConcurrencyWebSocketTests/ConcurrencyWebSocketTests.swift
@@ -1,6 +1,40 @@
 import Testing
+import Foundation
 @testable import ConcurrencyWebSocket
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct ConcurrencyWebSocketTests {
+    
+    let sut = URLSession(configuration: .default).webSocketTask(with: URL(string: "wss://echo.websocket.org")!)
+    
+    init() {
+        sut.resume()
+    }
+    
+    @Test func connect() async throws {
+        
+        let message = "Hello, World!"
+        
+        try await sut.send(.string(message))
+    
+        var received: [String] = []
+        
+        Task {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            #expect(received.contains(where: { $0 == message }))
+            sut.cancel()
+        }
+        
+        // Cancelling the WebSocket always throws an NSPOSIXErrorDomain error, which should be ignored.        
+        await #expect(throws: Error.self, performing: {
+            for try await echo in sut.stream() {
+                switch echo {
+                case .string(let echo):
+                    received.append(echo)
+                default:
+                    break
+                }
+            }
+        })
+        
+    }
 }
